72d0ecd583a851978dba84a199a42b29d104669c
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-16 19:27:43 -0300

    Fix CI

--- scripts/svn.sh
+++ scripts/svn.sh
@@ -5,7 +5,7 @@ svn co https://plugins.svn.wordpress.org/ltucillo-fixed-notices release_dir
 cp -r pre_release/* release_dir
 cd release_dir/trunk || exit
 svn stat | grep \? | awk '{print $2}' | xargs svn add
-svn ci -m ${TRAVIS_COMMIT_MESSAGE}
+svn ci -m $TRAVIS_COMMIT_MESSAGE
 cd ../../ || exit \
   && rm -rf release_dir
 svn cp https://plugins.svn.wordpress.org/ltucillo-fixed-notices/trunk https://plugins.svn.wordpress.org/ltucillo-fixed-notices/tags/$VERSION -m "$VERSION"
