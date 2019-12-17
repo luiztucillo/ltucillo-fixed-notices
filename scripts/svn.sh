e5ea6f99352920490904b2518ee83ea87242d7ec
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-17 13:25:24 -0300

    Fix CI

--- scripts/svn.sh
+++ scripts/svn.sh
@@ -3,11 +3,11 @@ VERSION=$(grep -E -o 'Version: ([0-9.]+)' ltucillo-fixed-notices.php | grep -E -
 mkdir release_dir
 svn co https://plugins.svn.wordpress.org/ltucillo-fixed-notices release_dir
 cp -r pre_release/* release_dir/trunk
-ls -la release_dir
 cd release_dir/trunk || exit
-ls -la
 svn stat | grep \? | awk '{print $2}' | xargs svn add
 svn ci -m $TRAVIS_COMMIT_MESSAGE
 cd ../../ || exit \
   && rm -rf release_dir
-svn cp https://plugins.svn.wordpress.org/ltucillo-fixed-notices/trunk https://plugins.svn.wordpress.org/ltucillo-fixed-notices/tags/$VERSION -m "$VERSION"
+svn cp --username $SVN_USERNAME --password $SVN_PASSWORD \
+  https://plugins.svn.wordpress.org/ltucillo-fixed-notices/trunk https://plugins.svn.wordpress.org/ltucillo-fixed-notices/tags/$VERSION \
+  -m "$VERSION"
