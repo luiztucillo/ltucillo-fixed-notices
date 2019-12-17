dc6cf04edecc877e2c0d1129e6f009980ec8025a
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-17 13:19:35 -0300

    Fix CI

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-FILENAME=$(TRAVIS_BRANCH)-$(TRAVIS_BUILD_NUMBER).tar.gz
+FILENAME=$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.tar.gz
+echo $FILENAME
 cd pre_release \
   && tar -cvz -f $FILENAME * \
   && mv $FILENAME ../ \
