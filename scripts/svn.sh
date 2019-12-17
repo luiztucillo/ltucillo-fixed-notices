136e4a290800c474ea9337fe24ce216c69cff123
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-17 13:43:32 -0300

    Test travis

--- scripts/svn.sh
+++ scripts/svn.sh
@@ -4,13 +4,15 @@ mkdir release_dir
 svn co https://plugins.svn.wordpress.org/ltucillo-fixed-notices release_dir
 cp -r pre_release/* release_dir/trunk
 cd release_dir/trunk || exit
-printf 'Trying to add new files...\n\n'
+printf '\n\nTrying to add new files...\n\n'
 svn stat | grep \? | awk '{print $2}' | xargs svn add
-printf 'Trying to check-in changes...\n\n'
-ls -la
+printf '\n\nTrying to check-in changes...\n\n'
+scn stat
 svn ci --non-interactive --username $SVN_USERNAME --password $SVN_PASSWORD -m $TRAVIS_COMMIT_MESSAGE
 cd ../../ || exit \
   && rm -rf release_dir
+svn rm --non-interactive --username $SVN_USERNAME --password $SVN_PASSWORD \
+  https://plugins.svn.wordpress.org/ltucillo-fixed-notices/tags/$VERSION
 svn cp --non-interactive --username $SVN_USERNAME --password $SVN_PASSWORD \
   https://plugins.svn.wordpress.org/ltucillo-fixed-notices/trunk https://plugins.svn.wordpress.org/ltucillo-fixed-notices/tags/$VERSION \
   -m "$VERSION"
