043778c1371b0208e9ffbaab6196a119d0bc8f29
“Luiz Tucillo” <luiz.tucillo@mercadolivre.com>
2019-12-19 14:24:15 -0300

    Travis

--- ltucillo-fixed-notices.php
+++ ltucillo-fixed-notices.php
@@ -3,7 +3,7 @@
  * Plugin Name: LTucillo Fixed Notices
  * Plugin URI: https://github.com/luiztucillo/ltucillo-fixed-notices
  * Description: Put fixed notices in admin based on user groups
- * Version: 0.1.6
+ * Version: 0.1.7
  * Author: Luiz Tucillo
  * Author URI: https://luiztucillo.com.br
  * Text Domain: ltucillo-fixed-notices
